424db04343cf325cbdea3fa2fe55506c4297373f
John-gr <20243ds060@utez.edu.mx>
2025-05-27 13:33:09 -0600

    Agregados setter y getters

--- Promedio27-5/src/Estudiante.java
+++ Promedio27-5/src/Estudiante.java
@@ -2,4 +2,26 @@ public class Estudiante {
     private String nombre;
     private String matricula;
     private double [] notas = new double [5];
+
+    //Setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+    public void setNotas(double [] notas) {
+        this.notas = notas;
+    }
+
+    //Getters
+    public String getNombre() {
+        return nombre;
+    }
+    public String getMatricula() {
+        return matricula;
+    }
+    public double[] getNotas() {
+        return notas;
+    }
 }
