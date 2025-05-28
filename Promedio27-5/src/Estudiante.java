e63294c34a6c1ea967d27d2b8b091493f4f38f62
John-gr <20243ds060@utez.edu.mx>
2025-05-27 18:35:53 -0600

    Correcion en el array de las notas

--- Promedio27-5/src/Estudiante.java
+++ Promedio27-5/src/Estudiante.java
@@ -11,8 +11,17 @@ public class Estudiante {
         this.matricula = matricula;
     }
     public void setNotas(double [] notas) {
-        this.notas = notas;
-    }
+        System.out.println("Ingrese las notas de la materia 1\n");
+        this.notas[0] = notas[0];
+        System.out.println("Ingrese las notas de la materia 2\n");
+        this.notas[1] = notas[1];
+        System.out.println("Ingrese las notas de la materia 3\n");
+        this.notas[2] = notas[2];
+        System.out.println("Ingrese las notas de la materia 4\n");
+        this.notas[3] = notas[3];
+        System.out.println("Ingrese las notas de la materia 5\n");
+        this.notas[4] = notas[4];
+        }
 
     //Getters
     public String getNombre() {
