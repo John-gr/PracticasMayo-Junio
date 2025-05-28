ba4f1134e573b57f205438cb040ae6b8924aaab9
John-gr <20243ds060@utez.edu.mx>
2025-05-28 07:58:34 -0600

    Agregado en main las instancias

--- U1_T1_07_Computadora/src/Main.java
+++ U1_T1_07_Computadora/src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("Hello, World!");
+        Computadora computadora = new Computadora();
+        computadora.setModelo("IBM");
+        computadora.encender();
+        computadora.encender();
+        computadora.apagar();
     }
 }
