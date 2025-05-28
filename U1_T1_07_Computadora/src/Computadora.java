7a2f25ceadc74d1b2e56c665a3d203c344ee3778
John-gr <20243ds060@utez.edu.mx>
2025-05-28 07:55:19 -0600

    Creados getters y set de modelo

--- U1_T1_07_Computadora/src/Computadora.java
+++ U1_T1_07_Computadora/src/Computadora.java
@@ -2,4 +2,20 @@ public class Computadora {
     private String marca;
     private String modelo;
     private boolean encendido;
+    
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public boolean getEncendido() {
+        return encendido;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
 }
