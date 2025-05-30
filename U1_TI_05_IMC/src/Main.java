a03da2f7552963cfb4846b6660389e9b730a02b7
John-gr <20243ds060@utez.edu.mx>
2025-05-30 09:10:31 -0600

    Agregado en main scanners de datos

--- U1_TI_05_IMC/src/Main.java
+++ U1_TI_05_IMC/src/Main.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Ingrese su nombre");
+        Scanner sc = new Scanner(System.in);
+        String nombre = sc.nextLine();
+        System.out.println("Ingrese su altura");
+        Scanner sc1 = new Scanner(System.in);
+        double altura=sc.nextDouble();
+        System.out.println("Ingrese su peso");
+        Scanner sc2 = new Scanner(System.in);
+        double peso=sc.nextDouble();
     }
 }
