7e8aeb5fc959ab64ff50b8f1d431fcfa3692d889
John-gr <20243ds060@utez.edu.mx>
2025-05-30 08:47:32 -0600

    Agregados setters y getters

--- U1_TI_05_IMC/src/Persona.java
+++ U1_TI_05_IMC/src/Persona.java
@@ -2,4 +2,28 @@ public class Persona {
     private String nombre;
     private double altura;
     private double peso;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
 }
