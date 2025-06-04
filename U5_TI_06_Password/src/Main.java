5ba4703283423dd5038ea507e7e21f8ba6c3cc14
Jonathan <20243ds060@utez.edu.mx>
2025-06-04 08:16:45 -0600

    Agregado en main pedir datos

--- U5_TI_06_Password/src/Main.java
+++ U5_TI_06_Password/src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello, World!");
+        System.out.println("Ingrese su nombre de usuario");
+        Scanner sc=new Scanner(System.in);
+        String nombre=sc.nextLine();
+        System.out.println("Ingrese su contraseña");
+        String password=sc.nextLine();
     }
 }
