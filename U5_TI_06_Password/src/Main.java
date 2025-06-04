28d242b4025874e0ec849f9f150239a42eaf53ad
John-gr <20243ds060@utez.edu.mx>
2025-06-04 08:57:45 -0600

    Correciones

--- U5_TI_06_Password/src/Main.java
+++ U5_TI_06_Password/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Ingrese su nombre de usuario");
@@ -5,10 +7,18 @@ public class Main {
         String nombre=sc.nextLine();
         System.out.println("Ingrese su contraseña");
         String password=sc.nextLine();
+        System.out.println("Ingrese de nuevo su contraseña");
+        String auth=sc.nextLine();
 
         UsuarioSeguro usuario=new UsuarioSeguro();
-        usuario.setNombreUsuario(usuario);
+        usuario.setNombreUsuario(nombre);
         usuario.setPassword(password);
-        System.out.println(usuario.nombreUsuario()+usuario.password());
+        if(usuario.userAuthentication(auth)){
+        System.out.println("Acceso concedido");
+        }
+        else {
+            System.out.println("Acceso denegado");
+        }
+
     }
 }
--- U5_TI_06_Password/src/UsuarioSeguro.java
+++ U5_TI_06_Password/src/UsuarioSeguro.java
@@ -21,27 +21,37 @@ public class UsuarioSeguro {
     public void setPassword(String password) {
         boolean verificadoMay = false;
         boolean verificadoMin = false;
+        boolean verificadoDig = false;
         if(password.length()<8){
             System.out.println("Ingrese minimo 8 caracteres");
         }
         else{
             for(int i=0;i<password.length();i++){
-                if((Character.isUpperCase(password.charAt(i)))==true){
+                if((Character.isUpperCase(password.charAt(i)))){
                     verificadoMay=true;
                 }
-                else if((Character.isLowerCase(password.charAt(i)))==true){
+                else if((Character.isLowerCase(password.charAt(i)))){
                     verificadoMin=true;
                 }
+                else if((Character.isDigit(password.charAt(i)))){
+                    verificadoDig=true;
+                }
             }
-            if(verificadoMay==true&&verificadoMin==true){
+            if(verificadoMay==true&&verificadoMin==true&&verificadoDig==true){
                 this.password = password;
             }
             else{
-                System.out.println("Ingrese al menos una mayuscula y una minuscula");
+                System.out.println("Ingrese al menos una mayuscula, una minuscula y un digito");
             }
         }
     }
 
-
-
+    public boolean userAuthentication(String intentPassword){
+        if(intentPassword.equals(this.password)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
