af9670bfdf3cdd6d55e634abe217fd92d4ff5f9a
Jonathan <20243ds060@utez.edu.mx>
2025-06-04 07:56:41 -0600

    Actualizado setName para evitar campos vacios

--- U5_TI_06_Password/src/UsuarioSeguro.java
+++ U5_TI_06_Password/src/UsuarioSeguro.java
@@ -6,8 +6,17 @@ public class UsuarioSeguro {
         return nombreUsuario;
     }
 
-    public void setNombreUsuario(nombreUsuario){
-        this.nombreUsuario=nombreUsuario;
+    public void setNombreUsuario(String nombreUsuario){
+        for (int i=0;i<nombreUsuario.length();i++){
+            if (nombreUsuario.charAt(i)==" "){
+                System.out.println("Ingrese un nombre valido");
+            }
+        }
+        if(nombreUsuario == null) {
+            System.out.println("Ingrese un nombre valido");
+        } else {
+            this.nombreUsuario=nombreUsuario;
+        }
     }
 
     public String getPassword() {
