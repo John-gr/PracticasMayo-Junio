730fb7130c6732c0a33a6ee9c00c906e0fd2b023
John-gr <20243ds060@utez.edu.mx>
2025-06-06 05:20:30 -0600

    Main argumentos

--- U1_TI_07_EjercicioPractica/src/Main.java
+++ U1_TI_07_EjercicioPractica/src/Main.java
@@ -2,7 +2,24 @@ public class Main {
     public static void main(String[] args) {
         Inventario inv = new Inventario();
         inv.sizeProducto(3);
-        inv.setProducto("Lapiz",1,5);
-        inv.agregarProducto(inv)
+        Producto prodc = new Producto();
+        prodc.setProducto("Lapiz","1",5);
+        inv.agregarProducto(prodc);
+        System.out.println(inv.totalItems());
+        prodc.setProducto("Goma","2",8);
+        inv.agregarProducto(prodc);
+        System.out.println(inv.totalItems());
+        prodc.setProducto("Regla","3",2);
+        inv.agregarProducto(prodc);
+        System.out.println(inv.totalItems());
+        prodc.setProducto("Color","4",10);
+        if(inv.agregarProducto(prodc)==false){
+            System.out.println("Fallo, ya esta lleno");
+        }
+        System.out.println("Producto nombre:"+inv.buscar("1"));
+        System.out.println("Producto nombre:"+inv.buscar("5"));
+        inv.eliminarProducto("2");
+        inv.agregarProducto(prodc);
+        System.out.println(inv.totalItems());
     }
 }
