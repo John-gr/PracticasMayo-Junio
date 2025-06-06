532f0674ece89d8075eeff907d5257c99ccdfc7a
John-gr <20243ds060@utez.edu.mx>
2025-06-06 03:32:36 -0600

    Creados getters y setters base

--- U1_TI_07_EjercicioPractica/src/Producto.java
+++ U1_TI_07_EjercicioPractica/src/Producto.java
@@ -3,4 +3,28 @@ public class Producto {
     private String nombre;
     private int cantidad;
 
+    public String getCodigo(){
+        return codigo;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public int getCantidad(){
+        return cantidad;
+    }
+
+    public void setCodigo(String codigo){
+        this.codigo=codigo;
+    }
+
+    public void setName(String nombre){
+        this.nombre=nombre;
+    }
+
+    public void setCantidad(int cantidad){
+        this.cantidad=cantidad;
+    }
+
 }
