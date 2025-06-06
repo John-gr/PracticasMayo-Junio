7e0ca52d51eae82ba78ff5bfd027370526a23926
John-gr <20243ds060@utez.edu.mx>
2025-06-06 03:48:42 -0600

    Creada la clase inventario con atributo

---
+++ U1_TI_07_EjercicioPractica/src/Inventario.java
@@ -0,0 +1,13 @@
+public class Inventario {
+    private int[]producto;
+
+    public int[] getProducto() {
+        return producto;
+    }
+
+    public void sizeProducto(int tamaño) {
+        this.producto=new int[tamaño];
+    }
+
+
+}
