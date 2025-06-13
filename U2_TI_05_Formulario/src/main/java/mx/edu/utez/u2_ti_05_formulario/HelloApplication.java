8ee10a0028164baf979065ad1d6cb866f487e298
John-gr <20243ds060@utez.edu.mx>
2025-06-13 09:39:19 -0600

    Arreglados imports

--- U2_TI_05_Formulario/src/main/java/mx/edu/utez/u2_ti_05_formulario/HelloApplication.java
+++ U2_TI_05_Formulario/src/main/java/mx/edu/utez/u2_ti_05_formulario/HelloApplication.java
@@ -1,8 +1,16 @@
 package mx.edu.utez.u2_ti_05_formulario;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,6 +18,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
 
         //Datos personales
         Label lblNombre = new Label("Nombre");
@@ -24,11 +33,14 @@ public class HelloApplication extends Application {
         Button btnReset = new Button("Reset");
         Label lblResultado = new Label("Resultado");
 
+        GridPane root = new GridPane();
+        root.setAlignment(Pos.CENTER);
+
         btnCrear.setOnAction(e ->{
             if(lblNombre.isEmpty || lblEdad.isEmpty || lblApellido.isEmpty || cbRoles.isEmpty){
                 lblResultado.setText("Error, no campos vacios");
             } else{
-                
+
             }
 
         });
