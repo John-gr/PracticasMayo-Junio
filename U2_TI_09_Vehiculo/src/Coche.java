5c37e521e2664dd05ab14217241349c69da5d77c
Emicort17 <20223tn119@utez.edu.mx>
2025-06-18 07:59:53 -0600

    Creadas clases coche y motocicleta

---
+++ U2_TI_09_Vehiculo/src/Coche.java
@@ -0,0 +1,11 @@
+public class Coche extends Vehiculo {
+    private int numeroPuertas;
+
+    public int getNumeroPuertas() {
+        return numeroPuertas;
+    }
+
+    public void setNumeroPuertas(int numeroPuertas) {
+        this.numeroPuertas = numeroPuertas;
+    }
+}
---
+++ U2_TI_09_Vehiculo/src/Motocicleta.java
@@ -0,0 +1,11 @@
+public class Motocicleta extends Vehiculo {
+    private boolean tieneCaja;
+
+    public boolean isTieneCaja() {
+        return tieneCaja;
+    }
+
+    public void setTieneCaja(boolean tieneCaja) {
+        this.tieneCaja = tieneCaja;
+    }
+}
