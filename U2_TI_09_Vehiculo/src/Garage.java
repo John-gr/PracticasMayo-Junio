5b5626d32a64fa28cee61cdc3b24c6625b5d8b48
Emicort17 <20223tn119@utez.edu.mx>
2025-06-18 08:24:26 -0600

    Creadas clase garage con atributos y metodos

---
+++ U2_TI_09_Vehiculo/src/Garage.java
@@ -0,0 +1,42 @@
+public class Garage {
+    private Vehiculo[] vehiculos;
+
+    public Garage(int numEspacios){
+        this.vehiculos=new Vehiculo[numEspacios];
+    }
+    public boolean estacionar(Vehiculo vehiculo){
+        for(int i=0;i<this.vehiculos.length;i++){
+            if(vehiculos[i]==null){
+                vehiculos[i]=vehiculo;
+                return true;
+            }
+            else {
+                System.out.println("Ya esta lleno");
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public boolean retirar(String placaBuscar){
+        for(int i=0;i<this.vehiculos.length;i++){
+            if(vehiculos[i].getPlaca().equals(placaBuscar)){
+                vehiculos[i]=null;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int contarVehiculos() {
+        int contador=0;
+        for(int i=0;i<this.vehiculos.length;i++){
+            if(vehiculos[i]!=null){
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+
+}
